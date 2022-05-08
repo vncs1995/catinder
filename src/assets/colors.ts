742aa5c3dbe8954223b8f5774914019aac1991e0
Vinicius Viana <vncs1995@gmail.com>
2022-05-08 14:09:12 -0300

    feat: adding icons library, creating assets folder with colors and custom fonts

---
+++ src/assets/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  white: '#fff',
+  lightPink: '#EC537E',
+  pink: '#FD267D',
+  red: '#E16359',
+  green: '#6BD88E',
+  disableGrey: '#BFBFC0',
+  backgroundGrey: '#E3E3E4',
+  fontPrimary: '#434141',
+  fontSecondary: '#BFBFC0',
+};
---
+++ src/components/Icon.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {IconProps} from 'react-native-vector-icons/Icon';
+
+export const myIcon = (props: IconProps) => <Icon {...props} />;
